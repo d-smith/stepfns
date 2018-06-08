b06a2c4877b100dde07548ed265f33d17afff71b
d-smith <doug.smith.mail@gmail.com>
2018-06-08 06:31:58 -0700

    Added step b

--- moresteps/manysteps.js
+++ moresteps/manysteps.js
@@ -2,14 +2,19 @@
 const aSucceeds = (callback, event) => {
     let result = {
         status: 'ok',
-        details: 'nothing to share'
+        details: 'nothing to share',
+        stepAOutput1: 'a1',
+        stepAOutput2: false,
+        stepAOutput3: 123
     };
 
     event['step-a-output'] = result;
     callback(null, event);
 };
 
-
+const storeAOutput = async (stepAOut) => {
+    console.log(`store A output: ${JSON.stringify(stepAOut)}`);
+}
 
 const aFails = (callback, event) => {
     function StepAError(message) {
@@ -27,11 +32,19 @@ module.exports.stepA = (event, context, callback) => {
     console.log(`input: ${JSON.stringify(event)}`);
 
     //Flip a coin to succeed or fail.
-    if(Math.random() > 0.5) {
+    if(Math.random() > 0.15) {
         aSucceeds(callback, event);
     } else {
         aFails(callback, event);
     }
 
     
+};
+
+module.exports.stepB = (event, context, callback) => {
+    console.log(`input: ${JSON.stringify(event)}`);
+    storeAOutput(event['step-a-output']);    
+
+    //Return the event as output to make the data available downstream
+    callback(null, event);
 };
