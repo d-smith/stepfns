76e8d2175bac93959d594ac59350a2d7a36d879e
d-smith <doug.smith.mail@gmail.com>
2018-06-09 08:51:11 -0700

    Added bucket write, txn id gen, etc

---
+++ s3-for-process-data/README.md
@@ -0,0 +1,1 @@
+serverless plugin install --name serverless-step-functions
--- s3-for-process-data/start.js
+++ s3-for-process-data/start.js
@@ -1,6 +1,29 @@
-module.exports.startProcess = (event, context, callback) => {
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+const FlakeIdGen = require('flake-idgen')
+    , intformat = require('biguint-format')
+    , generator = new FlakeIdGen;
+
+
+module.exports.startProcess = async (event, context, callback) => {
     console.log(`event: ${JSON.stringify(event)}`);
     console.log(`context: ${JSON.stringify(context)}`);
-    console.log(`bucket: ${process.env.BUCKET_NAME}`)
-    callback(null, {statusCode: 200, body: "got it"});
+    console.log(`bucket: ${process.env.BUCKET_NAME}`);
+
+    let txnId = intformat(generator.next(), 'hex',{prefix:'0x'});
+    console.log(`txnId: ${txnId}`);
+    
+    let params = {
+        Body: event['body'],
+        Key: txnId,
+        ServerSideEncryption: "AES256",
+        Bucket: process.env.BUCKET_NAME
+    };
+
+    let response = await S3.putObject(params).promise();
+    console.log(response);
+
+    callback(null, {statusCode: 200, body: txnId});
 }
