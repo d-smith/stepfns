ac2b20c852a4a89606d5ebbad3e057064ba45500
d-smith <doug.smith.mail@gmail.com>
2018-06-10 14:20:27 -0700

    Added write of process output

--- s3-for-process-data/aprocess.js
+++ s3-for-process-data/aprocess.js
@@ -17,6 +17,29 @@ module.exports.stepA = async (event, context, callback) => {
 
     let input = s3response['Body'].toString();
     console.log(`input: ${input}`);
+    let processData = JSON.parse(input);
+    
+    //Write output to object
+    let result = {
+        status: 'ok',
+        details: 'nothing to share',
+        stepAOutput1: 'a1',
+        stepAOutput2: false,
+        stepAOutput3: 123
+    };
+
+    processData['step-a-output'] = result;
+
+    let putParams = {
+        Body: JSON.stringify(processData),
+        Key: key,
+        ServerSideEncryption: "AES256",
+        Bucket: process.env.BUCKET_NAME
+    };
+    console.log(`putParams: ${JSON.stringify(putParams)}`);
+
+    s3response = await S3.putObject(putParams).promise();
+    console.log(s3response);
 
     //Make process data available to the next downstream process
     callback(null, event);
