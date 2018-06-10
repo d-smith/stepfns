6aad1654fbcc678b63e3e4d7900506a04dbd8a99
d-smith <doug.smith.mail@gmail.com>
2018-06-10 09:41:41 -0700

    added read of process data from s3 bucket in step function task

--- s3-for-process-data/aprocess.js
+++ s3-for-process-data/aprocess.js
@@ -1,3 +1,23 @@
-module.exports.stepA = (event, context, callback) => {
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+module.exports.stepA = async (event, context, callback) => {
     console.log(`event: ${JSON.stringify(event)})`);
+
+    let key = event['processData'];
+    console.log(`process data via key ${key}`);
+
+    let params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: key
+    };
+
+    let s3response = await S3.getObject(params).promise();
+    console.log(s3response);
+
+    let input = s3response['Body'].toString();
+    console.log(`input: ${input}`);
+
+    //Make process data available to the next downstream process
+    callback(null, event);
 }
