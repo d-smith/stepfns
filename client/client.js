7cb4bc5057eb2164a16ed6c94f0b926dba1b74f8
Smith, Doug <doug.smith@fmr.com>
2018-06-13 07:08:42 -0700

    Added status poll

--- client/client.js
+++ client/client.js
@@ -29,11 +29,53 @@ if(process.argv.length != 3) {
     process.exit(1);
 }
 
+function poll(fn, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    var checkCondition = async function(resolve, reject) {
+        console.log('checkCondition');
+        // If the condition is met, we're done!
+        var result = await fn();
+        if(result) {
+            resolve(result);
+        }
+        // If the condition isn't met but the timeout hasn't elapsed, go again
+        else if (Number(new Date()) < endTime) {
+            setTimeout(checkCondition, interval, resolve, reject);
+        }
+        // Didn't match and too much time, reject!
+        else {
+            reject(new Error('timed out for ' + fn + ': ' + arguments));
+        }
+    };
+
+    return new Promise(checkCondition);
+}
+
+const finishCheckerForArn = (execArn) => {
+    return async () => {
+        let description = await stepFunctions.describeExecution({executionArn: execArn}).promise();
+        let status = description['status'];
+        console.log(`status: ${status}`);
+        return status !== 'RUNNING';
+    }
+}
+
+const waitForFinish = async (executionArn) => {
+    //let description = await stepFunctions.describeExecution({executionArn: executionArn}).promise();
+    //console.log(description);
+    checkForFinish = finishCheckerForArn(executionArn);
+    let x = await poll(checkForFinish, 30000, 250);
+}
+
 const doIt = async (arn) => {
     invokeResults = await invokeIt(arn);
 
-    executionArn = invokeResults['executionArn'];
+    let executionArn = invokeResults['executionArn'];
     console.log(executionArn);
+    waitForFinish(executionArn);
+    console.log('done');
 }
 
 doIt(process.argv[2]);
