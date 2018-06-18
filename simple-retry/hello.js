ae5073bf9bb485d7473610cfb1a36e012d8ce5be
d-smith <doug.smith.mail@gmail.com>
2018-06-18 07:56:35 -0700

    minor code example cleanup for simply-retry

--- simple-retry/hello.js
+++ simple-retry/hello.js
@@ -1,17 +1,23 @@
+class MySpecificError extends Error {
+    constructor(...args) {
+        super(...args)
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
 module.exports.hello = (event, context, callback) => {
 
-    if(Math.random() > 0.2) {
-        function MySpecificError(message) {
-            this.name = 'MySpecificError';
-            this.message = message;
-        };
-        MySpecificError.prototype = new Error();
-    
-        const error = new MySpecificError("no bueno");
+    console.log(`call context: ${JSON.stringify(context)}`);
 
+    if(Math.random() > 0.2) {
+        console.log('Random error - invoke callback with error');
+        const error = new MySpecificError("no bueno");
         callback(error);
         return;
     }
 
+    console.log("all ok");
     callback(null, "Hello");
 };
